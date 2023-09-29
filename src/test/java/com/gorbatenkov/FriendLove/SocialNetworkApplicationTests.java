7597df07fb35edeb53ee5acb83769e8b1dae6025
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2023-09-29 13:27:38 +0300

    Update data
    * Add an ER diagram
    * Add JUnit tests

---
+++ src/test/java/com/gorbatenkov/FriendLove/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorbatenkov.FriendLove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
